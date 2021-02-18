3253c1c9ae1f11bed60070dfd8b642b5735e4945
Pattov <patriitovar95@gmail.com>
2021-02-18 16:44:11 +0100

    deberesakgebra

--- BD/Ejerciciodeconsulta.sql
+++ BD/Ejerciciodeconsulta.sql
@@ -39,9 +39,50 @@ VALUES ('P1','CARLOS', 20, 'SEVILLA'),
 ('P5','EVA',30, 'CACERES');
 
 INSERT INTO componentes 
-VALUES ('C1','X3A', 20, 'SEVILLA'),
-('C2','JUAN', 10, 'MADRID'),
-('C3','JOSÃ‰', 30,'SEVILLA'),
-('C4','INMA', 20, 'SEVILLA'),
-('C5','EVA',30, 'CACERES');
+VALUES ('C1','X3A','ROJO', 12,'SEVILLA'),
+('C2','B85','VERDE', 17,'MADRID'),
+('C3','C4B','AZUL', 17,'MALAGA'),
+('C4','C4B','ROJO', 14,'SEVILLA'),
+('C5','VT8','AZUL', 12,'MADRID'),
+('C6','C30','ROJO', 19,'SEVILLA');
 
+INSERT INTO articulos
+VALUES('T1','CLASIFICADORA','MADRID'),
+('T2','PERFORADORA','MALAGA'),
+('T3','LECTORA','CACERES'),
+('T4','CONSOLA','CACERES'),
+('T5','MEZCLADORA','SEVILLA'),
+('T6','TERMINAL','BARCELONA'),
+('T7','CINTA','SEVILLA');
+
+INSERT INTO envios
+VALUES('P1','C1','T1',200),
+('P1','C1','T4',700),
+('P2','C3','T1',400),
+('P2','C3','T2',200),
+('P2','C3','T3',200),
+('P2','C3','T4',500),
+('P2','C3','T5',600),
+('P2','C3','T6',400),
+('P2','C3','T7',800),
+('P2','C5','T2',100),
+('P3','C3','T1',200),
+('P3','C4','T2',500),
+('P4','C6','T3',300),
+('P4','C6','T7',300),
+('P5','C2','T2',200),
+('P5','C2','T4',100),
+('P5','C5','T4',500),
+('P5','C5','T7',100),
+('P5','C6','T2',200),
+('P5','C1','T4',100),
+('P5','C3','T4',200),
+('P5','C4','T4',800),
+('P5','C5','T5',400),
+('P5','C6','T4',500);
+
+Obtener todos los detalles de todos los articulos de CACERES
+
+    SELECT id_t, tnombre, ciudad
+        FROM articulos
+        WHERE ciudad='CACERES';
