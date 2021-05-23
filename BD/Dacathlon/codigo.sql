f9254913da220dbec84908b551e84be0b529e380
Alejandro0120 <73612152+Alejandro0120@users.noreply.github.com>
2021-05-23 19:58:28 +0200

    Update codigo.sql

--- BD/Dacathlon/codigo.sql
+++ BD/Dacathlon/codigo.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS dacathlon;
 
 CREATE DATABASE `dacathlon` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
+
 USE `dacathlon`;
 
 CREATE TABLE `dacathlon`.`personas` (
@@ -34,8 +35,8 @@ CREATE TABLE `dacathlon`.`empleados` (
 
 CREATE TABLE `secciones` (
   `idSeccion` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `idEmpleado` int(11) DEFAULT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  `idEmpleado` int(11) NOT NULL,
   PRIMARY KEY (`idSeccion`),
   FOREIGN KEY (`idEmpleado`) 
     REFERENCES `empleados` (`idEmpleado`)
@@ -43,14 +44,14 @@ CREATE TABLE `secciones` (
 
 CREATE TABLE `dacathlon`.`productos` (
   `idProducto` INT NOT NULL AUTO_INCREMENT,
-  `codProducto` VARCHAR(45) NULL,
-  `nombre` VARCHAR(45) NULL,
+  `codProducto` VARCHAR(45) NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
   `descripcion` VARCHAR(45) NULL,
-  `precio_unitario` DECIMAL(7,2) NULL,
+  `precio_unitario` DECIMAL(7,2) NOT NULL,
   `fabricante` VARCHAR(45) NULL,
   `marca` VARCHAR(45) NULL,
   `modelo` VARCHAR(45) NULL,
-  `idSeccion` INT NULL,
+  `idSeccion` INT NOT NULL,
   PRIMARY KEY (`idProducto`),
     FOREIGN KEY (`idSeccion`)
     REFERENCES `dacathlon`.`secciones` (`idSeccion`)
@@ -67,12 +68,12 @@ CREATE TABLE `dacathlon`.`ventas`(
 );
 
 CREATE TABLE `dacathlon`.`categorias`(
-  `idCategoria` INT NOT NULL PRIMARY KEY,
+  `idCategoria` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `descripcion` VARCHAR(45) NULL
 );
 
 CREATE TABLE `dacathlon`.`cat_pro`(
-  `id_categoria` VARCHAR(4) NOT NULL,
+  `id_categoria` INT NOT NULL,
   `id_producto` INT NOT NULL,
   PRIMARY KEY(`id_categoria`, `id_producto`),
   FOREIGN KEY (`id_categoria`) 
@@ -83,7 +84,7 @@ CREATE TABLE `dacathlon`.`cat_pro`(
 
 CREATE TABLE `dacathlon`.`caracteristicas`(
   `idCaracteristica` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id_categoria` VARCHAR(4) NOT NULL,
+  `id_categoria` INT NOT NULL,
   `nombre` VARCHAR(100),
   FOREIGN KEY (`id_categoria`)
       REFERENCES `dacathlon`.`categorias` (`idCategoria`)
@@ -92,7 +93,7 @@ CREATE TABLE `dacathlon`.`caracteristicas`(
 CREATE TABLE `dacathlon`.`car_pro`(
   `id_producto` INT NOT NULL,
   `id_caracteristica` INT NOT NULL,
-  `valor` INT NULL,
+  `valor` VARCHAR(50) NULL,
   PRIMARY KEY(`id_producto`, `id_caracteristica`),
   FOREIGN KEY (`id_producto`)
       REFERENCES `dacathlon`.`productos` (`idProducto`),
@@ -106,8 +107,8 @@ CREATE TABLE `dacathlon`.`posicionVenta`(
   `id_producto` INT NOT NULL,
   `cantidad` INT NULL,
   `importeTotal` DECIMAL(7,2) NULL,
-  `importeDeIva` DECIMAL(7,2) NULL,
-  `importeConIva` DECIMAL(7,2) NULL,
+  `importeDeIva` DECIMAL(7,2) null,
+  `importeConIva` DECIMAL(7,2) null,
   PRIMARY KEY(`posicion`, `id_venta`, `id_producto`),
   FOREIGN KEY (`id_venta`)
     REFERENCES `dacathlon`.`ventas` (`idVenta`),
@@ -119,14 +120,71 @@ INSERT INTO personas (nif, nombre) VALUES
 ('58423232B', 'Juancho'),
 ('83492323A', 'Marta'),
 ('44532435G', 'Alexys'),
-('17342043J', 'Pilar')
-('16504348K', 'Felix')
-('14232849F', 'Catalina');
+('17342043J', 'Pilar'),
+('16504348K', 'Felix'),
+('14232849F', 'Catalina'),
+('16984348K', 'Sara'),
+('16984348K', 'Jaime'),
+('16984348K', 'Ramon');
 
-INSERT INTO clientes VALUES
+INSERT INTO clientes (id_persona) VALUES
 (3),(5);
 
-INSERT INTO categorias VALUES
-('C001', 'Calzado deportivo'),
-('C002', 'Calzado de montaña');
+INSERT INTO empleados VALUES 
+(1,6,15000,null),
+(2,3,5000,1),
+(3,7,3500,1),
+(4,1,1500,3),
+(5,8,900,4),
+(6,9,450,4);
 
+INSERT INTO secciones VALUES 
+(1,'Montania',6),
+(2,'Deporte',5),
+(3,'Pesca',2),
+(4,'Caza',4);
+
+INSERT INTO `productos` (codProducto, nombre, precio_unitario, idSeccion) VALUES
+('X938290B', 'Nice Air Mux', 155, 1),
+('X938291B', 'Nice Air Mux', 155, 1),
+('X938292B', 'Nice Air Mux', 155, 1),
+('H327848J','Cania de Pesca Nuzman', 45.99, 3);
+
+INSERT INTO ventas (idVenta, id_Cliente, fecha, hora) VALUES 
+(202100001, 1, '2021-05-21', '17:35:05'),
+(202100011, 2, '2021-05-22', '11:12:45');
+
+INSERT INTO categorias (descripcion) VALUES
+('Calzado de montania'),
+('Cañas Golden');
+
+INSERT INTO cat_pro VALUES
+(1, 1),
+(2, 2);
+
+INSERT INTO `caracteristicas` (id_categoria, nombre) VALUES
+(1, 'Talla'),
+(1, 'Color'),
+(1, 'Material'),
+(2, 'Color'),
+(2, 'Material');
+
+INSERT INTO car_pro VALUES
+(1, 1, '39'),
+(1, 2, 'Verde'),
+(1, 3, 'Sintetica'),
+(2, 1, '40'),
+(2, 2, 'Negro'),
+(2, 3, 'Sintetica'),
+(3, 1, '41'),
+(3, 2, 'Blanca'),
+(3, 3, 'Sintetica'),
+(4, 4, 'Oro'),
+(4, 5, 'Acero');
+
+INSERT INTO posicionVenta (id_venta, id_producto, cantidad, importeTotal) VALUES 
+(202100001, 1, 1, 155.00),
+(202100011, 1, 2, 310.00),
+(202100011, 4, 1, 45.99),
+(202100011, 2, 1, 155.00),
+(202100011, 3, 1, 155.00);
