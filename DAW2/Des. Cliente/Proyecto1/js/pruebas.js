e881f64b72390be33f3c77321dcbff3d8fc193b1
Pattov <patriitovar95@gamil.com>
2021-09-24 17:58:00 +0200

    semana1

---
+++ DAW2/Des. Cliente/Proyecto1/js/pruebas.js
@@ -0,0 +1,17 @@
+// objeto.propiedasdelobjeto
+      /**
+       *
+       *
+       * @param {number} a 
+       * @param {number} b
+       * @return {number} 
+       */
+      function Suma(a, b) {
+        return a + b;
+      }
+      // crear un punto de interrupcion en las herramientas de desarrollo
+      debugger;
+      // declaro una variable
+      let x = 5,
+        y = 6;
+      console.log(Suma(x, y));
