5a499394b1c21944126072c0491bece0bedf712c
Pattov <patriitovar95@gamil.com>
2021-10-04 20:33:22 +0200

    JavaScript

--- DAW2/Des. Cliente/ejerciciosTema2/js/ejercicioDos.js
+++ DAW2/Des. Cliente/ejerciciosTema2/js/ejercicioDos.js
@@ -1,8 +1,13 @@
 let numeros=[5,4,8,2,1,3,9,6,7,0];
-
-for (let index = 0; index < numeros.length; index++) {
-    let num = numeros[index];
-    if(numeros[index]===index){
-        console.log(num);
+let contador=0;
+do{
+    for (let i = 0; i < numeros.length; i++) {
+        if(numeros[i]==i){
+            console.log(i);
+            contador++;
+        }
+        if(contador==numeros.length){
+            break;
+        }
     }
-}
+} while (true);
