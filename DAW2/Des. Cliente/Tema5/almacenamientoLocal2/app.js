39416be318381ee4d35d95fedb00433e053c5473
Pattov <patriitovar95@gamil.com>
2021-11-08 21:04:55 +0100

    javaScript

---
+++ DAW2/Des. Cliente/Tema5/almacenamientoLocal2/app.js
@@ -0,0 +1,16 @@
+
+
+//capturar valores del formulario
+document.getElementById("btnAgregar").onclick=function () {
+    // verificar que existe item agenda[] (array vacio)
+
+    //sino creo el item (almacenamiento local)
+}
+
+//instacion objeto apunte
+
+//recupero el item agenda -> convertimos en array
+
+//agregar apunte en array
+
+//volver a convertir el array en item con el apunte (AL)
