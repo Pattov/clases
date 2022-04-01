48058df4d8cb86cc13907c0e202a594ede475322
Pattov <patriitovar95@gamil.com>
2022-04-01 17:18:12 +0100

    Hola

---
+++ DAW2/Des. Cliente/EVA3/proyecto/calculadora.js
@@ -0,0 +1,12 @@
+//FUNCIONES
+function cambiar() {
+    document.getElementById("txt_pantalla").innerHTML = "1";
+}
+
+
+//DECLARAR VARIABLES
+let variable;
+
+//Ejecutar codigo
+document.querySelectorAll("#numero") = cambiar;
+document.getElementById("btn_uno").onclick = cambiar;
