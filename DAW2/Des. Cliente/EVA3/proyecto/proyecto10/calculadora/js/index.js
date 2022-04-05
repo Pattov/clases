83caea6caef76a146426083bd84b063111483a8a
Pattov <patriitovar95@gamil.com>
2022-04-05 19:15:37 +0200

    ultimo

--- DAW2/Des. Cliente/EVA3/proyecto/proyecto10/calculadora/js/index.js
+++ DAW2/Des. Cliente/EVA3/proyecto/proyecto10/calculadora/js/index.js
@@ -7,6 +7,12 @@
  * @param {*} numero - Numero tecleado
  */
 function agregarNumero(numero){
+    if(numero!=operacionPantalla.textContent.slice(-1)){
+        // incluimos el signo
+    
+        operacionPantalla.textContent = operacionPantalla.textContent + numero;
+
+    }
         // incluimos el numero
         operacionPantalla.textContent = operacionPantalla.textContent + numero;
 
@@ -16,7 +22,7 @@ function agregarNumero(numero){
  * Esta funcion nos permite introducir valores en la expresion a calcular
  * Excepciones:
  *      No nos permité introducir dos signos iguales seguidos
- *      No nos permité introducir otro operador detras de 
+ *      No nos permité introducir otro operador detras de / o *
  *
  * @param {*} valor - Operador tecleado
  */
@@ -34,7 +40,9 @@ function SelectOperador(valor) {
 function BorrarTodo() {
     operacionPantalla.textContent = '';
     resultadoPantalla.textContent = ''; 
+    acumuladorPosiciones = 0;
 }
+
 /**
  * con la funcion que tenemos opera según la prioridad de la operacion
  * @param {} operacion - operacion almacenada en la parte de la Pantalla
@@ -44,6 +52,24 @@ function Calcular(operacion) {
     return eval(operacion);
 }
 
+function Excepciones(caso) {
+    switch (caso) {
+        case 1:
+            //si lo ultimo de la variable es un signo de operacion lo quita
+            if(operacionPantalla.textContent.slice(-1)=='+'||operacionPantalla.textContent.slice(-1)=='-'||operacionPantalla.textContent.slice(-1)=='*'||operacionPantalla.textContent.slice(-1)=='/'){
+                return operacionPantalla.textContent=operacionPantalla.textContent.slice(0,(operacionPantalla.textContent.length-1));
+            }
+            break;
+        case 2:
+            //Si lo primero que se pulsa es un signo añadiremos 0 delante del signo
+            if (acumuladorPosiciones==0) {
+                operacionPantalla.textContent = "0";
+            }
+            break;
+        default:
+            break;
+    }
+}
 //DECLARAR VARIABLES
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
@@ -51,18 +77,22 @@ const botonBorrar = document.querySelector('#btn_borrar');
 const botonIgual = document.querySelector('#btn_igual');
 let operacionPantalla = document.getElementById('operacion');
 let resultadoPantalla = document.getElementById('resultado');
+let acumuladorPosiciones = 0;
 
 // EJECUCION CODIGO
 
 // Eventos Botones
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click',() => {
-       agregarNumero(boton.innerText);
+        acumuladorPosiciones++;
+        agregarNumero(boton.innerText);
     })
 });
 botonesOperadores.forEach(boton => {
     boton.addEventListener('click', () => {
         //Selecciona Operador
+        Excepciones(2);
+        acumuladorPosiciones++;
         SelectOperador(boton.innerText);
     })
 });
@@ -72,5 +102,7 @@ botonBorrar.addEventListener('click',() => {
  });
 
 botonIgual.addEventListener('click',() => {
+    Excepciones(1);
     resultadoPantalla.textContent = Calcular(operacionPantalla.textContent);
+
  });
