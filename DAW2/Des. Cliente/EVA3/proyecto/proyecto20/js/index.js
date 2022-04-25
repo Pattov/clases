21262a8d0cbab8464208cfbdd167c512ffbea92e
Patriciatvr <patricia.tvr6@gmail.com>
2022-04-25 13:46:07 +0200

    finde

--- DAW2/Des. Cliente/EVA3/proyecto/proyecto20/js/index.js
+++ DAW2/Des. Cliente/EVA3/proyecto/proyecto20/js/index.js
@@ -1,9 +1,12 @@
-// Comprobamos si el navegador soporta WebStorage 
-if(typeof(Storage)!=="undefined"){
-    //El navegador soporta WebStorage 
+// import agregaProducto from "./eventos";
 
-}else{
-    alert("El Programa va a presentar problemas. Usa otro Navegador");
-}
+    // Comprobamos si el navegador soporta WebStorage 
+    if(typeof(Storage)!=="undefined"){
+        //El navegador soporta WebStorage 
 
-
+        //Declaración de eventos
+        document.getElementById("agregaproducto").onclick = agregaProducto;
+    }else{
+        alert("El Programa va a presentar problemas. Usa otro Navegador");
+    }
+  
---
+++ DAW2/Des. Cliente/EVA3/proyecto/proyecto20/js/sidebar.js
@@ -0,0 +1,8 @@
+// sidebar.removeAttribute("visibility");
+// sidebar.setAttribute('visibility','hidden');
+function cerrar() {
+    sidebar.setAttribute('visibility','hidden');
+}
+let sidebar = document.getElementsByClassName('sidebarContainer');
+let btncerrar= document.querySelector('.side-bar');
+
