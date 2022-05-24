ddaf996e8d1d50b451792319064fdd11820a4cf1
Pattov <patriitovar95@gamil.com>
2022-05-24 19:07:33 +0200

    proyecto40 -5

--- DAW2/Des. Cliente/EVA3/proyecto/proyecto40/js/aldeanos.js
+++ DAW2/Des. Cliente/EVA3/proyecto/proyecto40/js/aldeanos.js
@@ -4,37 +4,85 @@
 //         <h5 class="card-title">Card title</h5>
 //     </div>
 // </div>
-function CrearCard() {
+function CrearCard(nombre, imagen) {
     //CREAR ESTRUCTURA
-    $('#listaAldeanos').append('div', {
+    $("#listaAldeanos").append(
+        $("<div>", {
             'class':'card',
-            'style':'width:18rem;',
-            html:'hola'
-        })
+            'style':'width:12rem;'
+        }).append(
+            $("<img>", {
+                'src': imagen,
+                'class':'card-img-top imgs',
+                'alt': nombre
+            })
+        ).append(
+            $("<div>", {
+                'class':'card-body'
+            }).append(
+                $("<h5>", {
+                    'class':'card-title',
+                    text: nombre
+                })
+            )
+        )
+    );
+    
+}
+function MostrarPersonalidad(personalidad) {
+    const url = 'https://api.nookipedia.com/villagers?game=nh&personality='+personalidad;
+    let request = new Request(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-KEY': 'f66873e9-3cf3-4b56-a17b-1987afe8c3de'
+        }
+    });
+
+
+    fetch(url,request)
+    .then((resp) => resp.json())
+    .then((data)=> {
+        console.log(data)
+    return data.map((vill)=> {
+        CrearCard(vill.name, vill.image_url);
+    })
+    })
+    .catch((error) => {
+    console.log(error);
+    });
 }
 
-const url = 'https://api.nookipedia.com/villagers?game=nh';
-let request = new Request(url, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'X-API-KEY': 'f66873e9-3cf3-4b56-a17b-1987afe8c3de'
-    }
-});
+function MostrarTodos() {
+    const url = 'https://api.nookipedia.com/villagers?game=nh';
+    let request = new Request(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-KEY': 'f66873e9-3cf3-4b56-a17b-1987afe8c3de'
+        }
+    });
 
 
-fetch(url,request)
-.then((resp) => resp.json())
-.then((data)=> {
-    console.log(data)
-return data.map((vill)=> {
-    console.log('hecho');
-    CrearCard;
-    // CrearCard(vill.name,vill.image_url);
-    $("#listaAldeanos").append($("</p>").html(`${vill.name}`));
-    // $("#listaAldeanos").append(`<img src=${vill.image_url}>`)
-})
-})
-.catch((error) => {
-console.log(error);
-});
+    fetch(url,request)
+    .then((resp) => resp.json())
+    .then((data)=> {
+        console.log(data)
+    return data.map((vill)=> {
+        CrearCard(vill.name, vill.image_url);
+    })
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+}
+
+
+$( "#personalidad" ).change((e)=>{
+
+}
+)
+
+
+
+// MostrarTodos();
