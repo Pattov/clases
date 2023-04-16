8f07011c3046cbbe6db21628dff3c50e4b195c27
Patriciatvr <patricia.tvr6@gmail.com>
2023-04-16 16:41:40 +0200

    menuFunciona

---
+++ DAW2/Proyecto/js/temas.js
@@ -0,0 +1,11 @@
+let toggle = document.getElementById("theme");
+let label = document.getElementById("label");
+toggle.addEventListener("change", (event) => {
+  let estado = event.target.checked;
+  document.body.classList.toggle("oscuro");
+  if (estado == true) {
+    label.innerHTML = "<i class='bx bxs-sun'></i>";
+} else {
+    label.innerHTML = "<i class='bx bxs-moon'></i>";
+  }
+});
